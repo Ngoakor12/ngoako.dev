a9f7dc7a3369d37768a2b20093ff51cbc7fa2e05
ngoakor12 <ngoakor12@gmail.com>
2022-01-08 17:05:46 +0200

    feat: added a hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <h1 className="hero-title">Ngoako Ramokgopa</h1>
+      <h2 className="hero-title gradient">{`<Frontend Developer/>`}</h2>
+      <p className="hero-subtitle">
+        I create functional and aesthetically pleasing digital experiences.
+      </p>
+      <section className="hero-buttons">
+        <button className="primary-btn">View work</button>
+        <button className="secondary-btn">Get in touch</button>
+      </section>
+    </section>
+  );
+}
+
+export default Hero;
