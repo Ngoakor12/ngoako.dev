da624abb7f4ad4067ff3d09d90800ef759fc586e
ngoakor12 <ngoakor12@gmail.com>
2022-01-08 14:32:14 +0200

    refactor: modify boilerplate text

--- src/App.js
+++ src/App.js
@@ -3,19 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Refactored boilerplate</h1>
     </div>
   );
 }
