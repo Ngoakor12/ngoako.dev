85a2d1dacfc159265682442e787c26a6c4a04960
ngoakor12 <ngoakor12@gmail.com>
2022-02-02 20:09:29 +0200

    feat: added Skills component

---
+++ src/components/SkillsList.js
@@ -0,0 +1,21 @@
+function SkillsList({ skills }) {
+  return (
+    <section className="skills-list">
+      {skills.map((skill, idx, array) => {
+        return (
+          //   <>
+          <div className="skill">
+            <p className="skill-name">
+              <span className="skill-icon">{skill.icon}</span>
+              {skill.name}
+            </p>
+            {idx !== array.length - 1 && <hr className="skill-separator" />}
+            {/* </> */}
+          </div>
+        );
+      })}
+    </section>
+  );
+}
+
+export default SkillsList;
