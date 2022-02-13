192fb62fec788cf84199ebd9f3b379acac732066
ngoakor12 <ngoakor12@gmail.com>
2022-02-13 17:52:44 +0200

    feat: added ProjectDetails component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import ScrollToTop from "./components/ScrollToTop";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop/>
       <App />
     </Router>
   </React.StrictMode>,
