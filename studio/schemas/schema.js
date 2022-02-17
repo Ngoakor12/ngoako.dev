bf311c9f46dab7beb46b84fb06a240c9d24b43d7
ngoakor12 <ngoakor12@gmail.com>
2022-02-17 13:50:44 +0200

    feat: added project schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -6,9 +6,10 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
 import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+// import category from './category'
+// import post from './post'
+// import author from './author'
+import project from './project'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -19,9 +20,10 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    post,
-    author,
-    category,
+    // post,
+    // author,
+    // category,
+    project,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
