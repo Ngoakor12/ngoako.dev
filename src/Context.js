7ccea60eb07876c3ac074b0863d800cb044fc0e9
ngoakor12 <ngoakor12@gmail.com>
2022-02-24 23:16:01 +0200

    feat: use sanity for about content

--- src/Context.js
+++ src/Context.js
@@ -1,17 +1,20 @@
 import { createContext, useState, useEffect } from "react";
 import sanityClient from "./client";
 import urlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
 
 const Context = createContext();
 
 function ContextProvider({ children }) {
   const [projects, setProjects] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  
+  const [aboutContent, setAboutContent] = useState(null);
+
   useEffect(() => {
     getAndSetProjects();
+    getAboutContent();
   }, [isLoading]);
-  
+
   function urlFor(source) {
     return urlBuilder({ projectId: "542oyksl", dataset: "production" }).image(
       source
@@ -27,33 +30,32 @@ function ContextProvider({ children }) {
   };
 
   async function getAndSetProjects() {
+    const query = `*[_type == "project"]{
+      title,
+      slug{current},
+      description,
+      technologies,
+      links,
+      details,
+      coverImage{
+        alt,
+        image{
+          asset{
+            _ref
+          }
+        }
+      },
+      mainImage{
+        alt,
+        image{
+          asset{
+            _ref
+          }
+        }
+      },
+    }`;
     try {
-      const response = await sanityClient.fetch(
-        `*[_type == "project"]{
-            title,
-            slug{current},
-            description,
-            technologies,
-            links,
-            details,
-            coverImage{
-              alt,
-              image{
-                asset{
-                  _ref
-                }
-              }
-            },
-            mainImage{
-              alt,
-              image{
-                asset{
-                  _ref
-                }
-              }
-            },
-            }`
-      );
+      const response = await sanityClient.fetch(query);
       const data = await response;
       setProjects(data);
       setIsLoading(false);
@@ -62,8 +64,39 @@ function ContextProvider({ children }) {
     }
   }
 
+  async function getAboutContent() {
+    const query = `*[_type == "about"]{
+      aboutImage{
+        alt,
+        image{
+          asset{
+            _ref
+          }
+        }
+      },
+      body
+    }`;
+    try {
+      const response = await sanityClient.fetch(query);
+      const data = await response;
+      setAboutContent(data[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <Context.Provider value={{ isLoading, projects,urlFor,serializers }}>
+    <Context.Provider
+      value={{
+        isLoading,
+        projects,
+        aboutContent,
+        urlFor,
+        serializers,
+        sanityClient,
+        BlockContent,
+      }}
+    >
       {children}
     </Context.Provider>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,45 @@
 import cv from "./assets/ngoako-cv.pdf";
+import { useContext } from "react";
+import { Context } from "../Context";
 
 function About() {
+  const { aboutContent, urlFor, BlockContent, serializers } =
+    useContext(Context);
+
   return (
     <section className="about">
       <header className="about-header">
         <h2 className="about-header-title">A bit about me</h2>
       </header>
-      <section className="about-content">
-        <div className="about-picture"></div>
-        <div className="about-body">
-          <p className="about-text">
-            I’m <strong>Ngoako Gift Ramokgopa</strong> (you can call me Gift if
-            you can’t pronounce my first name). I'm a{" "}
-            <strong>Frontend Developer</strong> from Ga-Ramokgopa a dusty
-            village in <strong>Polokwane, South Africa</strong>. I got into tech
-            via graphic design and slowly started learning code. I like playing{" "}
-            <strong>FIFA</strong>, watching <strong>football</strong>, and{" "}
-            <strong>trying new things</strong> in my free time.
-          </p>
-          <a href={cv} target="_blank" rel="noreferrer" className="primary-btn">
-            View CV
-          </a>
-        </div>
-      </section>
+      {aboutContent ? (
+        <section className="about-content">
+          <img
+            className="about-picture"
+            src={urlFor(aboutContent.aboutImage.image.asset)}
+            alt={aboutContent.aboutImage.alt}
+          />
+          <div className="about-body">
+            <p className="about-text">
+              <BlockContent
+                blocks={aboutContent.body}
+                projectId="542oyksl"
+                dataset="production"
+                serializers={serializers}
+              />
+            </p>
+            <a
+              href={cv}
+              target="_blank"
+              rel="noreferrer"
+              className="primary-btn"
+            >
+              View CV
+            </a>
+          </div>
+        </section>
+      ) : (
+        <p>Loading about...</p>
+      )}
     </section>
   );
 }
