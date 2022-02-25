a4367b4c041c3467f12bfb542877a426a1dfa8c4
ngoakor12 <ngoakor12@gmail.com>
2022-02-25 08:46:23 +0200

    fix: project details page direct navigation and reload error

--- src/components/projectDetails.js
+++ src/components/projectDetails.js
@@ -9,116 +9,120 @@ function ProjectDetails() {
   const project = projects.find((project) => project.slug.current === slug);
 
   return (
-    <>
-      <section className="project-details">
-        <header className="project-details-header">
-          <button onClick={() => navigate(-1)} className="projects-back-btn">
-            <span className="projects-back-btn-icon">
-              <svg
-                aria-hidden="true"
-                role="img"
-                width="20"
-                height="20"
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M21 11H6.414l5.293-5.293l-1.414-1.414L2.586 12l7.707 7.707l1.414-1.414L6.414 13H21z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </span>
-            Back to projects
-          </button>
-          <h1 className="project-details-heading">{project.title}</h1>
-          <p className="project-details-description">{project.description}</p>
-          <div className="project-links">
-            <div className="project-link">
-              <a href={project.links.live} className="live-site">
-                Live project
-                <span className="live-icon">
-                  <svg
-                    aria-hidden="true"
-                    role="img"
-                    width="28"
-                    height="28"
-                    preserveAspectRatio="xMidYMid meet"
-                    viewBox="0 0 24 24"
-                  >
-                    <g
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+    <section className="project-details">
+      {project ? (
+        <>
+          <header className="project-details-header">
+            <button onClick={() => navigate(-1)} className="projects-back-btn">
+              <span className="projects-back-btn-icon">
+                <svg
+                  aria-hidden="true"
+                  role="img"
+                  width="20"
+                  height="20"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M21 11H6.414l5.293-5.293l-1.414-1.414L2.586 12l7.707 7.707l1.414-1.414L6.414 13H21z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </span>
+              Back to projects
+            </button>
+            <h1 className="project-details-heading">{project.title}</h1>
+            <p className="project-details-description">{project.description}</p>
+            <div className="project-links">
+              <div className="project-link">
+                <a href={project.links.live} className="live-site">
+                  Live project
+                  <span className="live-icon">
+                    <svg
+                      aria-hidden="true"
+                      role="img"
+                      width="28"
+                      height="28"
+                      preserveAspectRatio="xMidYMid meet"
+                      viewBox="0 0 24 24"
                     >
-                      <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                      <path d="M15 3h6v6"></path>
-                      <path d="M10 14L21 3"></path>
-                    </g>
-                  </svg>
-                </span>
-              </a>
-              <hr className="link-border" />
-            </div>
-            <div className="project-link">
-              <a href={project.links.code} className="source-code">
-                Code repository
-                <span className="code-icon">
-                  <svg
-                    aria-hidden="true"
-                    role="img"
-                    width="28"
-                    height="28"
-                    preserveAspectRatio="xMidYMid meet"
-                    viewBox="0 0 24 24"
-                  >
-                    <g
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="M16 18l6-6l-6-6"></path>
-                      <path d="M8 6l-6 6l6 6"></path>
-                    </g>
-                  </svg>
-                </span>
-              </a>
-              <hr className="link-border" />
-            </div>
-          </div>
-        </header>
-        <img
-          className="project-details-picture"
-          src={urlFor(project.mainImage.image.asset)}
-          alt={project.mainImage.alt}
-        />
-        {project.details.map(
-          (detail, index, array) =>
-            detail.body.length && (
-              <div className="project-detail">
-                <div className="project-detail-details">
-                  <h2>{detail.name}</h2>
-                  <div>
-                    <BlockContent
-                      blocks={detail.body}
-                      projectId="542oyksl"
-                      dataset="production"
-                      serializers={serializers}
-                    />
-                  </div>
-                </div>
-
-                {index !== array.length - 1 && (
-                  <hr className="details-border" />
-                )}
+                      <g
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                        <path d="M15 3h6v6"></path>
+                        <path d="M10 14L21 3"></path>
+                      </g>
+                    </svg>
+                  </span>
+                </a>
+                <hr className="link-border" />
               </div>
-            )
-        )}
-      </section>
-    </>
+              <div className="project-link">
+                <a href={project.links.code} className="source-code">
+                  Code repository
+                  <span className="code-icon">
+                    <svg
+                      aria-hidden="true"
+                      role="img"
+                      width="28"
+                      height="28"
+                      preserveAspectRatio="xMidYMid meet"
+                      viewBox="0 0 24 24"
+                    >
+                      <g
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="M16 18l6-6l-6-6"></path>
+                        <path d="M8 6l-6 6l6 6"></path>
+                      </g>
+                    </svg>
+                  </span>
+                </a>
+                <hr className="link-border" />
+              </div>
+            </div>
+          </header>
+          <img
+            className="project-details-picture"
+            src={urlFor(project.mainImage.image.asset)}
+            alt={project.mainImage.alt}
+          />
+          {project.details.map(
+            (detail, index, array) =>
+              detail.body.length && (
+                <div className="project-detail">
+                  <div className="project-detail-details">
+                    <h2>{detail.name}</h2>
+                    <div>
+                      <BlockContent
+                        blocks={detail.body}
+                        projectId="542oyksl"
+                        dataset="production"
+                        serializers={serializers}
+                      />
+                    </div>
+                  </div>
+
+                  {index !== array.length - 1 && (
+                    <hr className="details-border" />
+                  )}
+                </div>
+              )
+          )}
+        </>
+      ) : (
+        <p>Loading project...</p>
+      )}
+    </section>
   );
 }
 
