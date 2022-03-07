5c3883eb99f4f31fa9a6a8d232826156fa641db4
ngoakor12 <ngoakor12@gmail.com>
2022-03-07 18:28:18 +0200

    chore: update projects section link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,8 +15,14 @@ function Projects() {
       <header className="projects-header">
         <h2 className="projects-header-title">Projects</h2>
         <p className="projects-header-description">
-          These are some of the projects that showcases my skills. View more on
-          my github.
+          These are some of the projects that showcase some of my skills.{" "}
+          <a
+            href="https://github.com/Ngoakor12"
+            target="_blank"
+            rel="noreferrer"
+          >
+            View more on my github.
+          </a>
         </p>
       </header>
       <ProjectsList />
