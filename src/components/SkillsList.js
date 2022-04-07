62722d93aca60ae78a317c67cae3356b3a92394d
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-04-07 17:56:32 +0430

    Add years of experience

--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -2,12 +2,18 @@ function SkillsList({ skills }) {
   return (
     <section className="skills-list">
       {skills.map((skill, idx, array) => {
+        const startDate = new Date(skill.startTime);
+        const years = Math.floor((Date.now() - startDate.getTime())/(365*24*3600*1000));
+
         return (
           <div key={skill.name} className="skill">
-            <p className="skill-name">
-              <span className="skill-icon">{skill.icon}</span>
-              {skill.name}
-            </p>
+            <div className="skill-info">
+              <p className="skill-name">
+                <span className="skill-icon">{skill.icon}</span>
+                {skill.name}
+              </p>
+              <p>{years === 0 ? `<1 year` : `${years}+ years`}</p>
+            </div>
             {idx !== array.length - 1 && <hr className="skill-separator" />}
           </div>
         );
--- src/skills-data.js
+++ src/skills-data.js
@@ -15,42 +15,51 @@ const skillsData = {
     {
       name: "HTML",
       icon: htmlIcon,
+      startTime: "2018-04-07T13:17:48.864Z",
     },
     {
       name: "CSS",
       icon: cssIcon,
+      startTime: "2018-04-07T13:17:48.864Z",
     },
     {
       name: "JavaScript",
       icon: javascriptIcon,
+      startTime: "2021-02-08T13:17:48.864Z",
     },
     {
       name: "ReactJS",
       icon: reactIcon,
+      startTime: "2021-08-07T13:17:48.864Z",
     },
   ],
   backend: [
     {
       name: "NodeJS(Express)",
       icon: nodejsIcon,
+      startTime: "2021-10-07T01:17:48.864Z",
     },
     {
       name: "Firebase",
       icon: firebaseIcon,
+      startTime: "2021-10-07T01:17:48.864Z",
     },
   ],
   other: [
     {
       name: "Figma",
       icon: figmaIcon,
+      startTime: "2019-04-08T13:17:48.864Z",
     },
     {
       name: "Git(Github)",
       icon: githubIcon,
+      startTime: "2020-04-07T13:17:48.864Z",
     },
     {
       name: "Unit testing",
       icon: jasmineIcon,
+      startTime: "2019-04-08T13:17:48.864Z",
     },
   ],
 };
