ec472f1fbc6b0a622cdd393317750d2afd7f449c
ngoakor12 <ngoakor12@gmail.com>
2022-04-07 18:22:32 +0200

    chore: add skill start year

--- src/skills-data.js
+++ src/skills-data.js
@@ -15,42 +15,51 @@ const skillsData = {
     {
       name: "HTML",
       icon: htmlIcon,
+      startYear: 2018,
     },
     {
       name: "CSS",
       icon: cssIcon,
+      startYear: 2018,
     },
     {
       name: "JavaScript",
       icon: javascriptIcon,
+      startYear: 2020,
     },
     {
       name: "ReactJS",
       icon: reactIcon,
+      startYear: 2021,
     },
   ],
   backend: [
     {
       name: "NodeJS(Express)",
       icon: nodejsIcon,
+      startYear: 2021,
     },
     {
       name: "Firebase",
       icon: firebaseIcon,
+      startYear: 2022,
     },
   ],
   other: [
     {
       name: "Figma",
       icon: figmaIcon,
+      startYear: 2020,
     },
     {
       name: "Git(Github)",
       icon: githubIcon,
+      startYear: 2020,
     },
     {
       name: "Unit testing",
       icon: jasmineIcon,
+      startYear: 2020,
     },
   ],
 };
