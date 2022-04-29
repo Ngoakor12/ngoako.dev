c645a7299b76c68dff687593772c0b7c911fe27f
Rodrigo Bondoc <rbondoc@Rodrigos-MacBook-Air.local>
2022-04-28 23:23:35 -0500

    refactor: SkipButton to SkipSectionButton; fragment cleanup

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import ProjectsList from "./ProjectsList";
 import { useEffect, useContext } from "react";
 import { Context } from "../Context";
-import SkipButton from "./SkipButton";
+import SkipSectionButton from "./SkipSectionButton";
 
 function Projects() {
   const { setPageTitle } = useContext(Context);
@@ -13,7 +13,7 @@ function Projects() {
 
   return (
     <div className="section-wrapper">
-      <SkipButton skipTo={"skills"} children={"Skip Projects"} />     
+      <SkipSectionButton skipTo={"skills"} children={"Skip Projects"} />     
       <section className="projects" id="projects">
         <header className="projects-header">
           <h2 className="projects-header-title">Projects</h2>
