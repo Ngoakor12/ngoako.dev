147833eda4fc7760cbd4320732d4a3c27c4e4352
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2023-01-28 16:12:16 +0530

    Feat: Convert `App.js` to `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route
           path="/"
-          exact
           element={
             <>
               <Navigation />
